4ad0ae765926d8b0f4e1241b558365b0aa711aee
Admin Server <alfrandhy@gmail.com>
2025-08-01 16:20:01 +0700

    up to server

--- config/logging.php
+++ config/logging.php
@@ -69,8 +69,9 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
+            'permission' => 0775,
         ],
 
         'slack' => [
